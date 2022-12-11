c756541fa2fa006e117f3dd149b117d5b577a747
Alexander Ryazanov <110193564+AlexanderRyaz@users.noreply.github.com>
2022-12-11 14:20:07 +0300

    интеджерлист тест не работает, доделать!

--- src/test/java/org/example/IntegerListTest.java
+++ src/test/java/org/example/IntegerListTest.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class IntegerListTest extends MyListTest<IntegerList>{
     public IntegerListTest() {
         super(IntegerList::new);
+    }
+    @Test
+    void sortInsertion(){
+        Integer[]array=new Integer[]{1,5,4,3,2};
+        IntegerList.sortInsertion(array);
+        assertArrayEquals(new Integer[]{1,2,3,4,5},array);
     }
 }
